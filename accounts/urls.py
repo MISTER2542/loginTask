90ac0159b2904ab05ee61ea0700a2d600bd21e82
MISTER2542 <williangabriel2542.wglo@gmail.com>
2024-04-20 16:12:43 -0400

    logiTask

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('signup/', views.UserCreateAndLoginView.as_view(), name='signup'), 
     path('login/', LoginView.as_view(
         redirect_authenticated_user=True,
-        template_name='login.html'
+        template_name='accounts/login.html'
     ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('user_detail/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
